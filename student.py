a3cf3cd85c15728b4a4636a570a675645bbedabc
muganurakasha-ai <muganurakasha@gmail.com>
2025-11-22 09:33:57 +0530

    Create student.py

---
+++ student.py
@@ -0,0 +1,10 @@
+import sys
+if len(syn.avrg) !=3:
+  print("usage:python student.py <name>
+  sys.exit(1)
+script_name = sys.argyv[0]
+name = sys.arg[1]
+rollno = sys.argv[2]
+print ("script name: ",script_name>
+print ("student name: ", name>
+print ("roll number: ",rollno>
